075f9048a5450d700e901d86d1e36e7a29a39cef
Sahil-charde <sahilcharde1701@gmail.com>
2026-07-05 15:46:52 +0530

    bitwise added

---
+++ basics/bitwise.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a=10,b=3;
+    // cout<<(a<<1)<<endl;     // (<<) <- ye number ko 2 se multiple karta 
+    // cout<<(a<<2)<<endl;     // (<<) <- ye number ko 2*2 se multiple karta 
+
+    // cout<<(a>>1)<<endl;     // (>>) <- ye number ko 2 se divide karta aur Q return karta 
+    // cout<<(a>>2)<<endl;     // (>>) <- ye number ko 2*2 se divide karta aur Q return karta
+
+   
+
+}
