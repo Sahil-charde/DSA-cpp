36fb2d42af279128f292c23018ead18f80d99d6c
Sahil-charde <sahilcharde1701@gmail.com>
2026-07-23 00:04:05 +0530

    rotate an array by k times in right direction added

---
+++ arrays/Rotate_right_by_Ktimes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> nums={1,2,3,4,5,6,7};
+    int k=3;
+    int d = nums.size();
+    k=k%d;
+    reverse(nums.begin(),nums.end());
+    reverse(nums.begin(),nums.begin()+k);
+    reverse(nums.begin()+k,nums.end());
+    
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<"\t";
+    }
+
+}
